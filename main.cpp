a73df81622311ef7fcebb374af404816675320a6
Roman Shkurdyuk <romansh0106@gmail.com>
2025-11-01 14:36:39 +0300

    Feat: Add array and thread count input

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@
 using namespace std;
 
 int main()
-{
-    cout << "Лабораторная работа №3. Начало." << endl;
+{   
+    cout << "Enter the size of the array" << endl;
+    int size_of_initial_array;
+    cin >> size_of_initial_array;
+    vector<int> initial_array(size_of_initial_array, 0);
+    cout << "Enter the number of marker threads" << endl;
+    int number_of_marker_threads;
+    cin >> number_of_marker_threads;
     return 0;
 }
