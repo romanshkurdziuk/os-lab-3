4cd6ea1b4342ad5a18be73167fb2cb55fce252f0
Roman Shkurdyuk <romansh0106@gmail.com>
2025-11-02 12:14:28 +0300

    Feat: Implement thread creation and initial synchronization

--- main.cpp
+++ main.cpp
@@ -4,14 +4,78 @@
 
 using namespace std;
 
+CRITICAL_SECTION cs;
+
+struct MarkerThreedParams
+{
+    int id;
+    vector<int> *arr;
+    HANDLE hStartEvent;
+};
+
+DWORD WINAPI marker_thread(LPVOID params)
+{
+    MarkerThreedParams *threadParams = (MarkerThreedParams*)params;
+    int id = threadParams->id;
+    vector<int>& arr = *(threadParams->arr);
+    HANDLE hStartEvent = threadParams->hStartEvent;
+    EnterCriticalSection(&cs);
+    cout << "Marker thread # " << id << " It has been created and is waiting for a signal to start working." << endl;
+    LeaveCriticalSection(&cs);
+    WaitForSingleObject(hStartEvent, INFINITE);
+    delete threadParams;
+    return 0;
+}
+
 int main()
 {   
+
     cout << "Enter the size of the array" << endl;
-    int size_of_initial_array;
-    cin >> size_of_initial_array;
-    vector<int> initial_array(size_of_initial_array, 0);
+    int arraySize;
+    cin >> arraySize;
+    vector<int> arr(arraySize, 0);
     cout << "Enter the number of marker threads" << endl;
-    int number_of_marker_threads;
-    cin >> number_of_marker_threads;
+    int markersCount;
+    cin >> markersCount;
+
+    InitializeCriticalSection(&cs);
+    vector<HANDLE> hMarkers(markersCount);
+    HANDLE hStartEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+    if (hStartEvent == NULL) 
+    {
+        cerr << "Failed to create a start event." << endl;
+        return 1;
+    }
+    for (int i = 0; i < markersCount; ++i)
+    {
+        MarkerThreedParams *params = new MarkerThreedParams();
+        params->id = i;
+        params->arr = &arr;
+        params->hStartEvent = hStartEvent;
+        hMarkers[i] = CreateThread(NULL, 0, marker_thread, params, 0, NULL);
+        if (hMarkers[i] == NULL)
+        {
+            cerr << "Failed to create a thread # " << i << endl;
+            return 1;
+        }
+    } 
+    cout << "ALL threads created. Please press Enter to get started." << endl;
+    cin.get();
+    cin.get();
+    cout << "\n--- START ---" << endl;
+    SetEvent(hStartEvent);
+    
+
+    WaitForMultipleObjects(markersCount, hMarkers.data(), TRUE, INFINITE);
+    cout << "\n--- THE WORK IS COMPLETED ---" << endl;
+
+    CloseHandle(hStartEvent);
+    for (int i = 0; i < markersCount; ++i) 
+    {
+        CloseHandle(hMarkers[i]);
+    }
+    DeleteCriticalSection(&cs);
+    system("pause");
     return 0;
 }
